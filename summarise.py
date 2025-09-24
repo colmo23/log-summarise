c264ff4b3626eb40488d6c64cd7110c2c56908cf
Colm O'Shea <colm.oshea@adaptivemobile.com>
2025-09-24 15:31:07 +0100

    option to not normalise

--- summarise.py
+++ summarise.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import re
 import sys
 from collections import defaultdict
@@ -5,13 +7,21 @@ import argparse
 
 
 def parse_options():
-    parser = argparse.ArgumentParser(description="Summarise one of more log files. Normalises lines to remove numbers. Provides a count of each instance of a normalised line in reverse order.")
+    parser = argparse.ArgumentParser(
+        description="Summarise one of more log files. Normalises lines to remove numbers. Provides a count of each instance of a normalised line in reverse order."
+    )
     parser.add_argument(
         "-f",
         "--files",
         dest="files_list",
         nargs="+",
         help="One or more files to summarise.",
+    )
+    parser.add_argument(
+        "--do-not-normalise",
+        dest="do_not_normalise",
+        action="store_true",
+        help="Do not normalise lines before summarising",
     )
     args = parser.parse_args()
     if not args.files_list:
@@ -34,13 +44,16 @@ line_count = 0
 log_counts = defaultdict(int)
 
 
-def process_a_file(filename):
+def process_a_file(filename, normalise_lines=True):
     global line_count
     global log_counts
     with open(filename, "r") as fh:
         for line in fh:
             line_count += 1
-            normalised_line = normalise_line(line)
+            if normalise_lines:
+                normalised_line = normalise_line(line)
+            else:
+                normalised_line = line
             log_counts[normalised_line] += 1
 
 
@@ -60,12 +73,16 @@ def print_summary():
 
 if __name__ == "__main__":
     args = parse_options()
+    if args.do_not_normalise:
+        normalise = False
+    else:
+        normalise = True
 
     processed_files = []
     error_files = []
     for file in args.files_list:
         try:
-            process_a_file(file)
+            process_a_file(file, normalise)
             processed_files.append(file)
         except (UnicodeDecodeError, IsADirectoryError):
             print("error with {file}")
